54f0a4c6659ea5ff3617484d705758570f3c38d2
v.khvostov <v.khvostov@tinkoff.ru>
2019-06-13 15:59:47 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase';
 import './App.css';
+var config = {
+    apiKey: "AIzaSyCVvijZimUxlVaB_lRA3ewKv2C0jwsSK7Y",
+    authDomain: "agapovka-ccb77.firebaseapp.com",
+    databaseURL: "https://agapovka-ccb77.firebaseio.com",
+    projectId: "agapovka-ccb77",
+    storageBucket: "agapovka-ccb77.appspot.com",
+    messagingSenderId: "523006834768"
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(p) {
+        super(p);
+        this.state = {
+            messages: []
+        }
+    }
+    componentDidMount() {
+
+        firebase.initializeApp(config);
+        const database = firebase.database();
+
+        database.ref('chat').on('value', (snapshot) => {
+            const messages = snapshot.val();
+            this.setState({messages: Object.values(messages)})
+        })
+    }
+
+    render() {
+
+       return this.state.messages.map((message, i) => <div className='wrapper' key={i}>
+           <div className='incomingMessage message'>{message}</div>
+
+           <div className="outCommingMessage message">Как тебя зовут?</div>
+           <input placeholder='Введите текст' type="text"/>
+       </div>)
+   }
 }
 
 export default App;
