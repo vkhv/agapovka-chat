ebf3e576b4a98cfafde2d812c74833e82e89e43b
v.khvostov <v.khvostov@tinkoff.ru>
2019-06-14 19:15:50 +0300

    CI eblya

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import firebase from 'firebase';
 import './App.css';
-var config = {
-    apiKey: "AIzaSyCVvijZimUxlVaB_lRA3ewKv2C0jwsSK7Y",
-    authDomain: "agapovka-ccb77.firebaseapp.com",
-    databaseURL: "https://agapovka-ccb77.firebaseio.com",
-    projectId: "agapovka-ccb77",
-    storageBucket: "agapovka-ccb77.appspot.com",
-    messagingSenderId: "523006834768"
-};
+import config from './config';
 const ENTER_KEYCODE = 13;
 class App extends React.Component {
     constructor(p) {
---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCVvijZimUxlVaB_lRA3ewKv2C0jwsSK7Y",
+    authDomain: "agapovka-ccb77.firebaseapp.com",
+    databaseURL: "https://agapovka-ccb77.firebaseio.com",
+    projectId: "agapovka-ccb77",
+    storageBucket: "agapovka-ccb77.appspot.com",
+    messagingSenderId: "523006834768"
+};
